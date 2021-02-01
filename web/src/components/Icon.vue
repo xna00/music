f797a120d2ee818b05337a802f995c5257683021
xna <xna00@qq.com>
2021-02-01 23:49:29 +0800

    create Icon

---
+++ web/src/components/Icon.vue
@@ -0,0 +1,12 @@
+<template>
+  <svg class="icon">
+    <use :xlink:href="`#icon-${name}`"></use>
+  </svg>
+</template>
+<script lang="ts">
+export default {
+  props: {
+    name: { type: String },
+  },
+};
+</script>
