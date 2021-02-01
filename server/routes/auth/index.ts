7734da5c32166cc335e4e49834a0c635b52fa46f
xna <xna00@qq.com>
2021-02-01 18:11:09 +0800

    注册自动创建歌单

--- server/routes/auth/index.ts
+++ server/routes/auth/index.ts
@@ -1,5 +1,6 @@
 import express = require("express");
 import User from "../../models/User";
+import Mix from "../../models/Mix";
 import jwt from "jsonwebtoken";
 import assert = require("http-assert");
 import errorHandler from "../../middleware/errorHandler";
@@ -10,18 +11,16 @@ module.exports = (app) => {
   router.post("/register", async (req, res) => {
     const { username, password } = req.body;
     const newUser = await User.create({ username, password });
+    Mix.create({ name: "喜欢的音乐", owner: newUser._id });
     res.send(newUser);
   });
 
   router.post("/login", async (req, res) => {
     const { username, password } = req.body;
-
     const user = await User.findOne({ username }).select("+password");
     assert(user, 422, "用户不存在");
-    // 2.校验密码
     const isValid = require("bcrypt").compareSync(password, user!.password);
     assert(isValid, 422, "密码错误");
-    // // 3.返回token
     const token = jwt.sign({ id: user!._id }, app.get("secret"));
     res.send({ token });
   });
