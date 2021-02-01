4a79af1d4ba71463b22485ce3deb19daf293b49f
xna <xna00@qq.com>
2021-02-01 23:52:36 +0800

    主页 wip

---
+++ web/src/mobile/components/Layout.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="vh-100">
+    <header>
+      <slot name="header" />
+    </header>
+    <main>
+      <slot />
+    </main>
+    <footer></footer>
+  </div>
+</template>
+<style lang="scss" scoped></style>
--- web/src/mobile/views/Main.vue
+++ web/src/mobile/views/Main.vue
@@ -1,3 +1,27 @@
+<script lang="ts">
+import Layout from "../components/Layout.vue";
+import Header from "../components/Header.vue";
+import Icon from "../../components/Icon.vue";
+export default {
+  components: { Layout, Header, Icon },
+};
+</script>
 <template>
-    Main
+  <Layout>
+    <template v-slot:header>
+      <Header>
+        <div class="d-flex jc-between px-3 header">
+          <Icon name="menu" />
+          <Icon name="search" />
+        </div>
+      </Header>
+    </template>
+    
+  </Layout>
 </template>
+<style lang="scss" scoped>
+.header {
+  font-size: 1.6rem;
+  color: white;
+}
+</style>
