f389e38466d27c1f4e55e637452daa5bbd26dc87
xna <xna00@qq.com>
2021-02-04 19:08:04 +0800

    baseURL

--- web/src/lib/http/index.ts
+++ web/src/lib/http/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 const http = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: process.env.VUE_APP_API_URL || "/api",
 });
 
 http.interceptors.request.use((config) => {
@@ -10,4 +10,4 @@ http.interceptors.request.use((config) => {
   return config;
 });
 
-export default http
+export default http;
